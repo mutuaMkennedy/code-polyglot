9e3560d4a79200f433f142c9c827ff3bc1afa756
mutuamkennedy <mutuakennedy81@gmail.com>
2024-04-03 01:54:03 +0300

    feat: allow null values.

--- apps/route-radar/api/route_radar/stations/models.py
+++ apps/route-radar/api/route_radar/stations/models.py
@@ -2,15 +2,15 @@ from django.contrib.gis.db import models
 
 
 class Route(models.Model):
-    shape_id = models.CharField(max_length=254)
-    points = models.IntegerField()
-    trip_id = models.CharField(max_length=10)
-    head_sign = models.CharField(max_length=26)
-    direction = models.IntegerField()
-    route_id = models.CharField(max_length=11)
-    route_name = models.CharField(max_length=6)
-    route_long = models.CharField(max_length=79)
-    geom = models.MultiLineStringField(srid=4326)
+    shape_id = models.CharField(max_length=254, null=True)
+    points = models.IntegerField(null=True)
+    trip_id = models.CharField(max_length=10, null=True)
+    head_sign = models.CharField(max_length=26, null=True)
+    direction = models.IntegerField(null=True)
+    route_id = models.CharField(max_length=11, null=True)
+    route_name = models.CharField(max_length=6, null=True)
+    route_long = models.CharField(max_length=79, null=True)
+    geom = models.MultiLineStringField(srid=4326, null=True)
 
     class Meta:
         db_table = "route"
@@ -21,17 +21,17 @@ class Route(models.Model):
 
 
 class Station(models.Model):
-    stop_id = models.CharField(max_length=254)
-    stop_name = models.CharField(max_length=254)
-    stop_lat = models.FloatField()
-    stop_long = models.FloatField()
-    location_type = models.BigIntegerField()
-    parent_station = models.CharField(max_length=254)
-    trip_count = models.IntegerField()
-    trip_ids = models.CharField(max_length=251)
-    route_ids = models.CharField(max_length=254)
-    route_names = models.CharField(max_length=109)
-    geom = models.MultiPointField(srid=4326)
+    stop_id = models.CharField(max_length=254, null=True)
+    stop_name = models.CharField(max_length=254, null=True)
+    stop_lat = models.FloatField(null=True)
+    stop_long = models.FloatField(null=True)
+    location_type = models.BigIntegerField(null=True)
+    parent_station = models.CharField(max_length=254, null=True)
+    trip_count = models.IntegerField(null=True)
+    trip_ids = models.CharField(max_length=251, null=True)
+    route_ids = models.CharField(max_length=254, null=True)
+    route_names = models.CharField(max_length=109, null=True)
+    geom = models.MultiPointField(srid=4326, null=True)
 
     class Meta:
         db_table = "station"
