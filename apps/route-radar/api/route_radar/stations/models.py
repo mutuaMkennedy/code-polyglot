71654380dc87e3476bb5fd97bd91909d68854db2
mutuamkennedy <mutuakennedy81@gmail.com>
2024-04-02 20:52:55 +0300

    feat: add data tables.

--- apps/route-radar/api/route_radar/stations/models.py
+++ apps/route-radar/api/route_radar/stations/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Route(models.Model):
+    shape_id = models.CharField(max_length=254)
+    points = models.IntegerField()
+    trip_id = models.CharField(max_length=10)
+    head_sign = models.CharField(max_length=26)
+    direction = models.IntegerField()
+    route_id = models.CharField(max_length=11)
+    route_name = models.CharField(max_length=6)
+    route_long = models.CharField(max_length=79)
+    geom = models.MultiLineStringField(srid=4326)
+
+    class Meta:
+        db_table = "route"
+        verbose_name_plural = "Routes"
+
+    def __str__(self):
+        return f"{self.route_name} - {self.route_long}"
+
+
+class Station(models.Model):
+    stop_id = models.CharField(max_length=254)
+    stop_name = models.CharField(max_length=254)
+    stop_lat = models.FloatField()
+    stop_long = models.FloatField()
+    location_type = models.BigIntegerField()
+    parent_station = models.CharField(max_length=254)
+    trip_count = models.IntegerField()
+    trip_ids = models.CharField(max_length=251)
+    route_ids = models.CharField(max_length=254)
+    route_names = models.CharField(max_length=109)
+    geom = models.MultiPointField(srid=4326)
+
+    class Meta:
+        db_table = "station"
+        verbose_name_plural = "Stations"
+
+    def __str__(self):
+        return f"{self.stop_name} - {self.route_names}"
