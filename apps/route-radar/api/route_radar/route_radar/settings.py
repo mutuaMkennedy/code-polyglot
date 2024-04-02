921b48c51d3a197662b26052e21bda7c52c73a03
mutuamkennedy <mutuakennedy81@gmail.com>
2024-04-02 20:51:04 +0300

    fix: add db geospatial engine.

--- apps/route-radar/api/route_radar/route_radar/settings.py
+++ apps/route-radar/api/route_radar/route_radar/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "route_radar.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
